45d433796e95bd1ebf5f985ce744bac57bd73a8d
makoto2025 <aratamakoto2025@gmail.com>
2026-01-23 15:03:20 -0800

    exercise 16 completed

--- exercises/ex16.js
+++ exercises/ex16.js
@@ -1,5 +1,8 @@
 /*
-For this quiz, you're going to create a function called buildTriangle() that will accept an input (the triangle at its widest width) and will return the string representation of a triangle. See the example output below.
+? For this quiz, you're going to create a function called buildTriangle() 
+? that will accept an input (the triangle at its widest width) 
+? and will return the string representation of a triangle. 
+See the example output below.
 
 buildTriangle(10);
 Returns:
@@ -14,7 +17,9 @@ Returns:
 * * * * * * * * 
 * * * * * * * * * 
 * * * * * * * * * * 
-We've given you one function makeLine() to start with. The function takes in a line length, and builds a line of asterisks and returns the line with a newline character.
+We've given you one function makeLine() to start with. 
+The function takes in a line length, 
+and builds a line of asterisks and returns the line with a newline character.
 
 function makeLine(length) {
   var line = "";
@@ -25,13 +30,28 @@ function makeLine(length) {
 }
 You will need to call this makeLine() function in buildTriangle().
 
-This will be the most complicated program you've written yet, so take some time thinking through the problem before diving into the code. What tools will you need from your JavaScript tool belt? Professionals plan out their code before writing anything. Think through the steps your code will need to take and write them down in order. Then go through your list and convert each step into actual code. Good luck!
+This will be the most complicated program you've written yet, 
+so take some time thinking through the problem before diving into the code. 
+What tools will you need from your JavaScript tool belt? 
+Professionals plan out their code before writing anything. 
+Think through the steps your code will need to take and write them down in order. 
+Then go through your list and convert each step into actual code. Good luck!
 */
 
-function makeLine(length) {
+const makeLine = function (length) {
     var line = "";
     for (var j = 1; j <= length; j++) {
       line += "* "
     }
     return line + "\n";
 }
+
+const buildTriangle = function(width) {
+    let triangle = "";
+    for (let i = 1; i <= width; i++) {
+        triangle += makeLine(i);
+    }
+    return triangle;
+}
+
+console.log(buildTriangle(10));
