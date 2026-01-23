0eebf6ea7a041ad2884f60296685b537f874323e
makoto2025 <aratamakoto2025@gmail.com>
2026-01-23 11:06:36 -0800

    exercise 8 completed

--- exercises/ex08.js
+++ exercises/ex08.js
@@ -1,11 +1,12 @@
 /*
-"Fizzbuzz" is a famous interview question used in programming interviews. It goes something like this:
+"Fizzbuzz" is a famous interview question used in programming interviews. 
+It goes something like this:
 
-Loop through the numbers 1 to 100
-If the number is divisible by 3, print "Fizz"
-If the number is divisible by 5, print "Buzz"
-If the number is divisible by both 3 and 5, print "FizzBuzz"
-If the number is not divisible by 3 or 5, print the number
+- Loop through the numbers 1 to 100
+- If the number is divisible by 3, print "Fizz"
+- If the number is divisible by 5, print "Buzz"
+- If the number is divisible by both 3 and 5, print "FizzBuzz"
+- If the number is not divisible by 3 or 5, print the number
 
 TIP: A number x is divisible by a number y if the answer to x / y has a remainder of 0. 
 For example, 10 is divisible by 2 because 10 / 2 = 5 with no remainder. 
@@ -15,8 +16,18 @@ You can check if a number is divisible by another number by checking if x % y ==
 
 var x = 1;
 
-while (/* your stop condition goes here */) {
-    // check divisibility
-    // print Fizz, Buzz, or FizzBuzz
-    // increment x
+while (x <= 100) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("FizzBuzz");
+    }
+    else if (x % 3 === 0) {
+        console.log("Fizz")
+    }
+    else if (x % 5 === 0) {
+        console.log("Buzz")
+    }
+    else {
+        console.log(x)
+    }
+    x++
 }
