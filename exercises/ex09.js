c4016c06dba69791a503e1d2c13973e09a88d89d
makoto2025 <aratamakoto2025@gmail.com>
2026-01-23 11:23:35 -0800

    exercise 9 completed

--- exercises/ex09.js
+++ exercises/ex09.js
@@ -1,20 +1,21 @@
 /*
-NASA's countdown to launch includes checkpoints where NASA engineers complete certain technical tasks. During the final minute, NASA has 6 tasks to complete:
+NASA's countdown to launch includes checkpoints where NASA engineers complete certain technical tasks. 
+During the final minute, NASA has 6 tasks to complete:
 
-Orbiter transfers from ground to internal power (T-50 seconds)
-Ground launch sequencer is go for auto sequence start (T-31 seconds)
-Activate launch pad sound suppression system (T-16 seconds)
-Activate main engine hydrogen burnoff system (T-10 seconds)
-Main engine start (T-6 seconds)
-Solid rocket booster ignition and liftoff! (T-0 seconds)
+-Orbiter transfers from ground to internal power (T-50 seconds)
+-Ground launch sequencer is go for auto sequence start (T-31 seconds)
+-Activate launch pad sound suppression system (T-16 seconds)
+-Activate main engine hydrogen burnoff system (T-10 seconds)
+-Main engine start (T-6 seconds)
+-Solid rocket booster ignition and liftoff! (T-0 seconds)
 NOTE: "T-50 seconds" read as "T-minus 50 seconds".
 
-Directions:
-Write a while loop that counts down from 60 seconds and:
+? Directions:
+? Write a while loop that counts down from 60 seconds and:
 
-If there's a task being completed, it prints out the task
-If there is no task being completed, it prints out the time as T-x seconds
-Use the task and time descriptions described above.
+? If there's a task being completed, it prints out the task
+? If there is no task being completed, it prints out the time as T-x seconds
+? Use the task and time descriptions described above.
 
 Your output should look like the following:
 
@@ -31,3 +32,30 @@ T-2 seconds
 T-1 seconds
 Solid rocket booster ignition and liftoff!
 */
+
+let i = 60;
+
+while (i >= 0) {
+    if (i === 50) {
+        console.log("Orbiter transfers from ground to internal power");    
+    } 
+    else if (i === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    else if (i === 16) {
+        console.log("Activate launch pad sound suppression system");
+    }
+    else if (i === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    else if (i === 6) {
+        console.log("Main engine start");
+    }
+    else if (i === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    else {
+        console.log(`T-${i} seconds`);
+    }
+    i--;
+}
