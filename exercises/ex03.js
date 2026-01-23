ff1a001c7af959248fbee786dd8e13ec451a9295
makoto2025 <aratamakoto2025@gmail.com>
2026-01-22 16:04:58 -0800

    exercise 3 completed

--- exercises/ex03.js
+++ exercises/ex03.js
@@ -1,12 +1,16 @@
 /*
-A murder mystery is a game typically played at parties wherein one of the partygoers is secretly, and unknowingly, playing a murderer, and the other attendees must determine who among them is the criminal. It's a classic case of whodunnit.
+A murder mystery is a game typically played at parties wherein one of 
+the partygoers is secretly, and unknowingly, playing a murderer, 
+and the other attendees must determine who among them is the criminal. 
+It's a classic case of whodunnit.
 
-Since this might be your first time playing a murder mystery, we've simplified things quite a bit to make it easier. 
+Since this might be your first time playing a murder mystery,
+we've simplified things quite a bit to make it easier. 
 Here's what we know! In this murder mystery there are:
 
-four rooms: the ballroom, gallery, billiards room, and dining room,
-four weapons: poison, a trophy, a pool stick, and a knife,
-and four suspects: Mr. Parkes, Ms. Van Cleve, Mrs. Sparr, and Mr. Kalehoff.
+? four rooms: the ballroom, gallery, billiards room, and dining room,
+? four weapons: poison, a trophy, a pool stick, and a knife,
+? and four suspects: Mr. Parkes, Ms. Van Cleve, Mrs. Sparr, and Mr. Kalehoff.
 We also know that each weapon corresponds to a particular room, so...
 
 - the poison belongs to the ballroom,
@@ -29,7 +33,32 @@ Afterwards, use this template to print a message to the console if the mystery w
 
 __________ did it in the __________ with the __________!
 
-What goes into the three blank spaces? You can fill in the blanks with the name of the suspect, the room, and the weapon! For example, an output string may look like:
+What goes into the three blank spaces? 
+You can fill in the blanks with the name of the suspect, the room, and the weapon! 
+For example, an output string may look like:
 
 Mr. Parkes did it in the dining room with the knife!
 */
+
+const room = "dining room";
+let weapon;
+let suspect;
+
+if (room === "ballroom") {
+    weapon = "poison";
+    suspect = "Mr. Kalehoff";
+}
+else if (room === "gallery") {
+    weapon = "trophy";
+    suspect = "Ms. Van Cleve";
+}
+else if (room === "billiards room") {
+    weapon = "pool stick";
+    suspect = "Mrs. Sparr";
+}
+else if (room === "dining room") {
+    weapon = "knife";
+    suspect = "Mr. Parkes";
+}
+
+console.log(`${suspect} did it in the ${room} with the ${weapon}!`)
