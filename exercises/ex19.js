2a07ada76ecf017268ce4bee52b8b2163ae5265e
makoto2025 <aratamakoto2025@gmail.com>
2026-01-23 22:42:57 -0800

    exercise 19 completed

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -6,3 +6,18 @@ pass an inline function expression instead.
 emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
 Prints: "I am happy, haha!"
 */
+
+
+function emotions(myEmotion, laughFunction) { 
+    console.log(`I am ${myEmotion}, ${laughFunction(3)}`);
+}
+
+
+emotions("happy", function(num) {
+    let sound = ""
+    for (let i = 0; i < num; i++) {
+        sound += "ha";
+    }
+    return sound + "!";
+});
+
