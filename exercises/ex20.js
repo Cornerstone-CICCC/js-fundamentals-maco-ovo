ee932181557026929b4fbea11859a1757eb9cfcd
makoto2025 <aratamakoto2025@gmail.com>
2026-01-24 19:38:08 -0800

    exercise 20 completed

--- exercises/ex20.js
+++ exercises/ex20.js
@@ -1,5 +1,6 @@
 /*
-Create an array called crew to organize the Serenity’s crew and set it equal to the variables below . You don't need to type out the actual strings, just use the provided variables.
+Create an array called crew to organize the Serenity’s crew and set it equal to the variables below . 
+You don't need to type out the actual strings, just use the provided variables.
 
 var captain = "Mal";
 var second = "Zoe";
@@ -9,3 +10,13 @@ var mercenary = "Jayne";
 var mechanic = "Kaylee";
 Then, print the crew array to the console.
 */
+
+let captain = "Mal";
+let second = "Zoe";
+let pilot = "Wash";
+let companion = "Inara";
+let mercenary = "Jayne";
+let mechanic = "Kaylee";
+
+const crew = [captain, second, pilot, companion, mercenary, mechanic];
+console.log(crew);
