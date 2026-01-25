86f233d038701f2ffe54a75393b683b0c7b86a85
makoto2025 <aratamakoto2025@gmail.com>
2026-01-24 19:42:23 -0800

    exercise 21 completed

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -1,8 +1,16 @@
 /*
-Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+? tarting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
 
-var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+* var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
 TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+
+ const prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+ prices[0] = 10
+ prices[2] = 30
+ prices[6] = 70
+
+ console.log(prices);
