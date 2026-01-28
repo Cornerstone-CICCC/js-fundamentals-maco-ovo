2327eeff4fd37119cbcdd73311ada58a7fd2a710
makoto2025 <aratamakoto2025@gmail.com>
2026-01-27 20:20:16 -0800

    exercise 23 completed

--- exercises/ex23.js
+++ exercises/ex23.js
@@ -1,8 +1,26 @@
-/*
+
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
-Write a loop under each comment in the file.
-Under the first, write a while loop that prints out each item of ingredients.
-Under the second, write a for loop that does the same thing.
-And under the third, write any loop, while or for that prints out each item of ingredients but backwards (that is, the first should be "bananas" and the last "eggs").
-*/
+// Write a loop under each comment in the file.
+
+// Under the first, write a while loop that prints out each item of ingredients.
+let i = 0;
+
+while (i < ingredients.length) {
+    console.log(ingredients[i])
+    i++
+}
+
+
+// Under the second, write a for loop that does the same thing.
+for (i=0; i < ingredients.length; i++) {
+    console.log(ingredients[i])
+}
+
+
+// And under the third, write any loop, while or for that prints out each item of ingredients but backwards (that is, the first should be "bananas" and the last "eggs").
+
+for (i = ingredients.length - 1; i>=0; i--) {
+    console.log(ingredients[i]);
+}
+
