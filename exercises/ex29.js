6028fa325f3ab41a130a34281741e7f7f976ba4d
makoto2025 <aratamakoto2025@gmail.com>
2026-01-28 16:56:51 -0800

    exercise 29 completed

--- exercises/ex29.js
+++ exercises/ex29.js
@@ -7,11 +7,23 @@ var donuts = [
   { type: "Cider", cost: 1.59 },
   { type: "Boston Cream", cost: 5.99 }
 ];
+
 Directions:
-Use the for in to loop over the array and print out the following donut summaries using console.log.
+? Use the for in to loop over the array and print out the following donut summaries using console.log.
 
 Jelly donuts cost $1.22 each
 Chocolate donuts cost $2.45 each
 Cider donuts cost $1.59 each
 Boston Cream donuts cost $5.99 each
 */
+
+var donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+];
+
+for ( i in donuts ) {
+console.log(`${donuts[i].type} donuts cost $${donuts[i].cost} each`);
+};
