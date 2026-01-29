0eb96a93ab792445acc4dffea0fb5c469154adb5
makoto2025 <aratamakoto2025@gmail.com>
2026-01-28 16:48:48 -0800

    exercise 24 completed

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -1,7 +1,9 @@
 /*
 In this challenge, we will be building a function that determines the last index of an item in an array.
 
-When this function is given an array and a value, it should return the index of the last time the value occurs in the array. If the value never occurs, the function should return -1.
+? When this function is given an array and a value, 
+? it should return the index of the last time the value occurs in the array. 
+? If the value never occurs, the function should return -1.
 
 For example:
 
@@ -20,3 +22,19 @@ console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
 console.log(lastIndexOf([], 3), "=?", -1);
 
 */
+
+const lastIndexOf = (nums, targetNum) => {
+    let index = -1
+    for(i=0; i<nums.length; i++){
+        if(nums[i] === targetNum){
+            index = i
+        }
+    }
+    return index;
+}
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
